afa08d2f4db168e041df760364b81ef161046c02
Tanuj Garg <tanujgarg@y7mail.com>
2019-03-03 17:27:42 +0530

    i

---
+++ precache-manifest.501deea687a37af6006fd7dcb1c9c534.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "82208d12525a257abcf1",
+    "url": "/static/js/main.82208d12.chunk.js"
+  },
+  {
+    "revision": "76c0515c2cd601f31f29",
+    "url": "/static/js/2.76c0515c.chunk.js"
+  },
+  {
+    "revision": "82208d12525a257abcf1",
+    "url": "/static/css/main.0cc6565f.chunk.css"
+  },
+  {
+    "revision": "7272969d85e78f310475df1096939c81",
+    "url": "/index.html"
+  }
+];
